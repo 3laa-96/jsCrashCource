39c560c9f070361bdfe1c30953b3b7225c900e58
3laa <3laa.xvx@gmail.com>
2023-05-09 19:19:36 +0300

    0.2

--- index.js
+++ index.js
@@ -1,37 +1,65 @@
+// Defined variables
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+const computerMove = document.getElementById("computerMove");
+
+rock.addEventListener("click", function () {
+  game("rock");
+});
+paper.addEventListener("click", function () {
+  game("paper");
+});
+scissors.addEventListener("click", function () {
+  game("scissors");
+});
+
+let results = document.getElementById("results");
+let matchResult = "";
+let computerChoice = "";
+let computerWins = "0";
+let playerWins = "0";
 
 function game(myChoice) {
   let randomNum = Math.random();
-  let computerChoice = "";
+
+  // Randomizing computerChoice
   if (randomNum >= 0 && randomNum < 1 / 3) {
     computerChoice = "rock";
-    console.log("computer choice was rock");
+    computerMove.innerText = "Computer choose : rock";
   } else if (randomNum >= 1 / 3 && randomNum < 2 / 3) {
-    computerChoice = "Paper";
-    console.log("computer choice was paper");
+    computerChoice = "paper";
+    computerMove.innerText = "Computer choose : paper";
   } else {
     computerChoice = "scissors";
-    console.log("computer choice was scissors");
+    computerMove.innerText = "Computer choose : scissors";
   }
 
+  // Comparing computerChoice with myChoice
   if (myChoice === computerChoice) {
-    console.log("tie");
+    matchResult = "tie";
   } else if (myChoice === "rock" && computerChoice === "scissors") {
-    console.log("player won");
+    matchResult = "player won";
+    playerWins++;
   } else if (myChoice === "rock" && computerChoice === "paper") {
-    console.log("computer won");
+    matchResult = "computer won";
+    computerWins++;
   } else if (myChoice === "paper" && computerChoice === "scissors") {
-    console.log("computer won1");
+    matchResult = "computer won";
+    computerWins++;
   } else if (myChoice === "paper" && computerChoice === "rock") {
-    console.log("player won2");
+    matchResult = "player won";
+    matchResult = "computer won";
+    playerWins++;
   } else if (myChoice === "scissors" && computerChoice === "paper") {
-    console.log("player won3");
+    matchResult = "player won";
+    playerWins++;
   } else {
-    console.log("computer won4");
+    matchResult = "computer won";
+    computerWins++;
   }
-  computerChoice = "";
+  // announcing results
+  results.innerText = `Results: ${matchResult}, player total wins are [ ${playerWins} ], while computer total wins are [ ${computerWins} ].`;
 }
 
 // 3:25:46
