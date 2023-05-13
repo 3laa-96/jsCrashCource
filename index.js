2e37d5674df034403d7c871a9ee67f431ab6c6a1
3laa <3laa.xvx@gmail.com>
2023-05-13 11:11:21 +0300

    1.0

--- index.js
+++ index.js
@@ -2,16 +2,21 @@
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
-const Move = document.getElementById("Move");
-const results = document.getElementById("results");
-let matchResult = {
+const MovesDisplayed = document.getElementById("MovesDisplayed");
+const resultsDisplayed = document.getElementById("resultsDisplayed");
+const reset = document.getElementById("reset");
+let defaultMatchResult = {
   cpuMove: "",
   playerMove: "",
-  cpuWins: "0",
-  playerWins: "0",
-  ties: "0",
+  cpuWins: 0,
+  playerWins: 0,
+  ties: 0,
   result: "",
 };
+
+testDisplay();
+
+// console.log(typeof JSON.parse(localStorage.getItem("results")));
 
 rock.addEventListener("click", function () {
   matchResult.playerMove = "Rock";
@@ -24,6 +29,10 @@ paper.addEventListener("click", function () {
 scissors.addEventListener("click", function () {
   matchResult.playerMove = "Scissors";
   playGame(matchResult.playerMove);
+});
+reset.addEventListener("click", function () {
+  localStorage.removeItem("results");
+  testDisplay();
 });
 
 // Randomizing computer choice
@@ -39,23 +48,32 @@ function computerChoice() {
 }
 // main function for playing the game
 function playGame(playerMove) {
-  computerChoice();
   const cpuMove = computerChoice();
   // matchResult =
-  matchResult.playerMove === cpuMove
+  playerMove === cpuMove
     ? (matchResult.ties++, (matchResult.result = "tie"))
-    : matchResult.playerMove === "Rock"
+    : playerMove === "Rock"
     ? cpuMove === "Paper"
       ? (matchResult.cpuWins++, (matchResult.result = "computer won"))
       : (matchResult.playerWins++, (matchResult.result = "player won"))
-    : matchResult.playerMove === "Paper"
+    : playerMove === "Paper"
     ? cpuMove === "Rock"
       ? (matchResult.playerWins++, (matchResult.result = "player won"))
       : (matchResult.cpuWins++, (matchResult.result = "computer won"))
     : cpuMove === "Paper"
     ? (matchResult.playerWins++, (matchResult.result = "player won"))
     : (matchResult.cpuWins++, (matchResult.result = "computer won"));
+  localStorage.setItem("results", JSON.stringify(matchResult));
+  display();
+}
 
-  Move.innerText = `Moves : Computer choose ${matchResult.cpuMove} while player choose ${matchResult.playerMove}`;
-  results.innerText = `Results : ${matchResult.result} , player total wins are [ ${matchResult.playerWins} ] while computer total wins are [ ${matchResult.cpuWins} ].`;
+// Display results
+function testDisplay() {
+  JSON.parse(localStorage.getItem("results")) === null
+    ? ((matchResult = defaultMatchResult), display())
+    : ((matchResult = JSON.parse(localStorage.getItem("results"))), display());
+}
+function display() {
+  MovesDisplayed.innerText = `Moves : Computer choose ${matchResult.cpuMove} while player choose ${matchResult.playerMove}`;
+  resultsDisplayed.innerText = `Results : ${matchResult.result} , player total wins are [ ${matchResult.playerWins} ] while computer total wins are [ ${matchResult.cpuWins} ].`;
 }
